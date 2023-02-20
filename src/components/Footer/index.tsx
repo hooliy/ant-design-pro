d22d0f4d7f857ae2d05f201ccb4793c40a21dcfe
hooliy <m@hooliy.com>
2023-02-20 16:18:06 +0800

    登录页修改

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { DefaultFooter } from '@ant-design/pro-components';
 import '@umijs/max';
 import React from 'react';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'xxx公司 版权所有';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
@@ -11,26 +11,26 @@ const Footer: React.FC = () => {
         background: 'none',
       }}
       copyright={`${currentYear} ${defaultMessage}`}
-      links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
+      // links={[
+      //   {
+      //     key: 'Ant Design Pro',
+      //     title: 'Ant Design Pro',
+      //     href: 'https://pro.ant.design',
+      //     blankTarget: true,
+      //   },
+      //   {
+      //     key: 'github',
+      //     title: <GithubOutlined />,
+      //     href: 'https://github.com/ant-design/ant-design-pro',
+      //     blankTarget: true,
+      //   },
+      //   {
+      //     key: 'Ant Design',
+      //     title: 'Ant Design',
+      //     href: 'https://ant.design',
+      //     blankTarget: true,
+      //   },
+      // ]}
     />
   );
 };
--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -143,13 +143,13 @@ const Login: React.FC = () => {
             minWidth: 280,
             maxWidth: '75vw',
           }}
-          logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
-          subTitle={'Ant Design 是西湖区最具影响力的 Web 设计规范'}
+          // logo={<img alt="logo" src="/logo.svg" />}
+          title="xxxx云平台"
+          subTitle={<>&nbsp;</>}
           initialValues={{
             autoLogin: true,
           }}
-          actions={['其他登录方式 :', <ActionIcons key="icons" />]}
+          // actions={['其他登录方式 :', <ActionIcons key="icons" />]}
           onFinish={async (values) => {
             await handleSubmit(values as API.LoginParams);
           }}
